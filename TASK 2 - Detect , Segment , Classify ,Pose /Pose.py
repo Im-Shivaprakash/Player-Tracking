a10e3737e2252f8ae9029bff3e73a9cadb6e0a4b
Im-Shivaprakash <shivaprakashsrinivasaniam@gmail.com>
2023-08-18 10:31:24 +0530

    Detect , Segment , Classify , Pose

---
+++ TASK 2 - Detect , Segment , Classify ,Pose /Pose.py
@@ -0,0 +1,14 @@
+#import library
+from ultralytics import YOLO
+from PIL import Image
+
+#load model
+model = YOLO('yolov8n-pose.pt')
+
+#prediction
+result = model("/home/shivaprakash/Documents/git/YOLO/TASK 2 - Detect , Segment , Classify ,Pose /Yoga.jpeg")
+
+for r in result:
+    im_array = r.plot()  # plot a BGR numpy array of predictions
+    im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
+    im.show()  # show image

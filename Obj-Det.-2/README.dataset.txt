f625a3ba3ec11738266d6a8c4afc4964a7eb6002
Im-Shivaprakash <shivaprakashsrinivasaniam@gmail.com>
2023-10-05 16:08:55 +0530

    deleted

--- Obj-Det.-2/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# Obj Det. > 2023-08-24 10:53pm
-https://universe.roboflow.com/infinitrix/obj-det.
-
-Provided by a Roboflow user
-License: MIT
-
--- TASK 2 - Detect , Segment , Classify ,Pose /Track/Track without OpenCV.py
+++
@@ -1,9 +0,0 @@
-#TRACKING WITHOUT OPENCV
-
-from ultralytics import YOLO
-
-model = YOLO('yolov8n.pt') 
-
-# Perform tracking with the model
-results = model.track(source="https://www.youtube.com/watch?v=Y1jTEyb3wiI", show=True)
-
